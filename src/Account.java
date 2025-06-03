c8503a90e74d38becf845bcf66b8d1d134524baa
shalinipriya9447@gmail.com <shalinipriya9447@gmail.com>
2025-06-02 20:56:49 -0500

    value changed from 0 to 6

--- src/Account.java
+++ src/Account.java
@@ -1,7 +1,7 @@
 public class Account {
     private double balance;
     public void deposit(double amount) {
-        if (amount > 0) balance += amount;
+        if (amount > 6) balance += amount;
     }
     public double getBalance(){
         return balance;
